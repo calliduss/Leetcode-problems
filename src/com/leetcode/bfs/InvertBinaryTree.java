3cf5cf831d7f0a578a9e24e1f12ca4cee1e636a5
t.aibulatov <t.aibulatov@gmail.com>
2022-08-28 15:41:17 +0400

    invert binary tree recursively and iteratively

--- src/com/leetcode/bfs/InvertBinaryTree.java
+++ src/com/leetcode/bfs/InvertBinaryTree.java
@@ -14,25 +14,56 @@ import java.util.Queue;
  *        2       7                     7        2
  *      /   \   /   \                 /   \    /   \
  *     1    3  6     9               9     6  3     1
- * article: swap left and right nodes
+ * description: swap left and right nodes
  **/
 public class InvertBinaryTree {
 
-    public TreeNode invertTree(TreeNode root) {
+    public TreeNode invertBinaryTreeRecursively(TreeNode root) {
         if(root == null) {
             return root;
         }
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root.left);
-        queue.add(root.right);
+        // swap, i.e. change pointers
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
 
-        root.right = queue.poll();
-        root.left = queue.poll();
-
-        invertTree(root.left);
-        invertTree(root.right);
+        invertBinaryTreeRecursively(root.left);
+        invertBinaryTreeRecursively(root.right);
 
         return root;
+    }
+
+    public TreeNode invertBinaryTreeIteratively(TreeNode root) {
+        if(root == null) {
+            return root;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            swapNodes(current);
+
+            if(current.left != null) {
+                queue.add(current.left);
+            }
+
+            if(current.right != null) {
+                queue.add(current.right);
+            }
+        }
+        return root;
+    }
+
+    private void swapNodes(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+
+        // swap, i.e. change pointers
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
     }
 }
